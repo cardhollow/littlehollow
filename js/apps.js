20e20788e9448a2bc2a496f80ddadf9fe9999137
JCPL-CHXD <jcplchxd@gmail.com>
2026-09-01 21:07:04 +0800

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -21,6 +21,7 @@
         gamefinder:{title: "GAME FINDER",src:"app/gamefinder.html",w:360,h:340},
         doom:{title:"Doom",src:"https://js-dos.com/games/doom.exe.html",w:360,h:340},
         pacman:{title:"Pacman",src:"https://funhtml5games.com?embed=pacman",w:360,h:340},
+        GBA:{title:"GBA",src:"app/GBA.html",w:360,h:340},
         browser:{title:"BROWSER",src:"app/browser.html",w:360,h:340},
         imageeditor:{title:"imageeditor",src:"app/imageEditor.html",w:480,h:380},
         messenger:{title:"MESSENGER",src:"app/messenger.html",w:430,h:360},
