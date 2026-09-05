b103b7b0f9f32303c743fb8e2d7db65814d9280e
JCPL-CHXD <jcplchxd@gmail.com>
2026-09-05 23:15:24 +0800

    Update useraccount.js

--- js/useraccount.js
+++ js/useraccount.js
@@ -1,21 +1,26 @@
 let username = localStorage.getItem("littlehollow.username") || "";
 let pat = localStorage.getItem("littlehollow.pat") || "";
+let github = localStorage.getItem("littlehollow.github") || "";
 
 function saveUserAccount(){
     localStorage.setItem("littlehollow.username", username);
     localStorage.setItem("littlehollow.pat", pat);
+    localStorage.setItem("littlehollow.github", github);
 }
 
 function clearUserAccount(){
     username = "";
     pat = "";
+    github = "";
     localStorage.removeItem("littlehollow.username");
     localStorage.removeItem("littlehollow.pat");
+    localStorage.removeItem("littlehollow.github");
 }
 
 window.userAccount = {
     get username(){ return username; },
     get pat(){ return pat; },
+    get github(){ return github; },
     save: saveUserAccount,
     clear: clearUserAccount
 };
