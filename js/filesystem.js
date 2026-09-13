b36a1a5b2d518253a84c96dbe13a77d19ba52d58
JCPL-CHXD <jcplchxd@gmail.com>
2026-09-13 05:31:18 -0700

    Update filesystem.js

--- js/filesystem.js
+++ js/filesystem.js
@@ -6,7 +6,10 @@
             "Little Hollow Environment\nCreated by CHXD\n\nProtected system filesystem.\nFiles here cannot be modified or removed.",
 
         "chxd:/system/readme.txt":
-            "Welcome to Little Hollow.\nOpen Apps, File Manager, Notepad, Paint, Calculator, or Messenger."
+            "Welcome to Little Hollow.\nOpen Apps, File Manager, Notepad, Paint, Calculator, or Messenger.",
+
+        "chxd:/system/lullaby.pkp":
+            `{"instrument":"bell","volume":0.7,"octave":4}<[d:250](250)[d:250](250)[g:750](750)[d:250](250)[d:250](250)[g:750](750)[d:250](250)[g:250](250)[k:500](500)[j:500](500)[h:500](500)[h:500](500)[g:750](750)[s:250](250)[d:250](250)[f:750](750)[s:250](250)[d:250](250)[f:750](750)[s:250](250)[f:250](250)[j:500](500)[h:500](500)[g:500](500)[j:500](500)[k:1000]>`
     };
 
     const DEVICE_PREFIX = "chxd:/device/";
